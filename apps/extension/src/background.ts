93385ce1acca1e728d2b2b06dfe78feeae35ea3c
veltira <asistente.veltira@gmail.com>
2026-08-22 15:30:26 -0300

    fix(transcription): detect existing offscreen document with runtime contexts

--- apps/extension/src/background.ts
+++ apps/extension/src/background.ts
@@ -1,5 +1,14 @@
 import {registerBackground} from './background-core.js';
 import {registerRecoveryClaims} from './recovery-claims.js';
 
+const offscreen=chrome.offscreen as typeof chrome.offscreen&{hasDocument?:()=>Promise<boolean>};
+if(typeof offscreen.hasDocument!=='function'){
+  offscreen.hasDocument=async()=>{
+    const url=chrome.runtime.getURL('offscreen.html');
+    const contexts=await chrome.runtime.getContexts({contextTypes:['OFFSCREEN_DOCUMENT'],documentUrls:[url]});
+    return contexts.length>0;
+  };
+}
+
 registerRecoveryClaims(chrome);
 registerBackground(chrome);
